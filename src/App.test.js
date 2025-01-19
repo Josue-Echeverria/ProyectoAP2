20876a1193da93003c2810456015939c8774abd0
Josue <jecheverria@estudiantec.cr>
2025-01-18 18:50:51 -0600

    Tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { addEvento } from "./api/api";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe('Integration Tests', () => {
+  it('should respond with status 201', async () => {
+    const response = await addEvento('Test Event', 'Online', '2023-10-10', 'logo.png', 'material.pdf', 'creator123')
+    expect(response.status).toBe(201);
+  },50000);
 });
